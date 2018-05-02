1dcd2f34ff767968855f80b64dc7c99853ef9f69
gjavila <gjavila86@gmail.com>
2018-05-03 01:09:24 +0200

    new project for basic input in android

--- app/src/main/java/com/example/gjavila/justjava/MainActivity.java
+++ app/src/main/java/com/example/gjavila/justjava/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.gjavila.justjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void increment(View view) {
+        int quantity= 3;
+        display(quantity);
+    }
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void decrement(View view) {
+        int quantity= 2;
+        display(quantity);
+    }
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        int quantity= 2;
+        display(quantity);
+        displayPrice(quantity*5);
+    }
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
     }
 }
