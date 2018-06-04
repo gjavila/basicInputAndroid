605c8ffb57c6dc4fc6b0e6ffb44ed930b51af46f
gjavila <gjavila86@gmail.com>
2018-06-04 18:48:39 +0200

    use of styles and themes as well as changing function to the order method so it uses an email

--- app/src/main/java/com/example/gjavila/justjava/MainActivity.java
+++ app/src/main/java/com/example/gjavila/justjava/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.gjavila.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    int quantity= 0;
+    int quantity= 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +21,11 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the increment button is clicked.
      */
     public void increment(View view) {
+        if(quantity==100){
+            //error message for more than 100
+            Toast.makeText(this,"you cannot order more than 100 cups",Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity= quantity + 1;
         display(quantity);
     }
@@ -23,6 +33,12 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the decrement button is clicked.
      */
     public void decrement(View view) {
+
+        if(quantity==1){
+            //error message you for less than 1
+            Toast.makeText(this,"you cannot order less than 1 cups",Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity= quantity - 1;
         display(quantity);
     }
@@ -30,9 +46,24 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = calculatePrice();
-        String priceMessage=createOrderSummary(price);
-        displayMessage(priceMessage);
+        EditText nameText=(EditText) findViewById(R.id.nameField);
+        String customerName=nameText.getText().toString();
+        CheckBox whipecream=(CheckBox) findViewById(R.id.whipped_cream);
+        boolean hasWhipcream=whipecream.isChecked();
+        CheckBox chocolate=(CheckBox) findViewById(R.id.chocolate);
+        boolean hasChocolate=chocolate.isChecked();
+        int price = calculatePrice(hasWhipcream,hasChocolate);
+        String priceMessage=createOrderSummary(price, hasWhipcream, hasChocolate, customerName);
+
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for:"+ customerName);
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
 
 
     }
@@ -41,8 +72,10 @@ public class MainActivity extends AppCompatActivity {
     creates a summary of the order with the number off cofffees and price
 
     * */
-    public String createOrderSummary(int price){
-        String priceMessage="Name: Gera Avila";
+    public String createOrderSummary(int price, boolean haswhipcream, boolean haschocolate, String customerName){
+        String priceMessage="Name: "+customerName;
+        priceMessage= priceMessage+ "\nAdd whipp cream:" + haswhipcream;
+        priceMessage= priceMessage+ "\nAdd whipp cream:" + haschocolate;
         priceMessage= priceMessage+ "\nQuantity:" + quantity;
         priceMessage= priceMessage + "\nTotal $" + price;
         priceMessage= priceMessage+ "\nThank you!!!";
@@ -61,17 +94,20 @@ public class MainActivity extends AppCompatActivity {
     * this method will calculate the price
     * and return the price
     * */
-    public int calculatePrice(){
-    int price = quantity*5;
+    public int calculatePrice(boolean haswhipecream, boolean haschocolate){
+        int baseprice=5;
+        // add 1 dolar to the base price
+        if(haswhipecream== true){
+            baseprice=baseprice+1;
+        }
+        //add 2 dollar to the ende price for the coffee
+        if(haschocolate==true){
+            baseprice=baseprice+2;
+        }
+
+    int price = quantity*baseprice;
     return price;
     }
 
-    /**
-     * This method displays the given text on the screen.
-     */
 
-    private void displayMessage(String message) {
-        TextView orederSumary = (TextView) findViewById(R.id.Order_Summary_Text_View);
-        orederSumary.setText(message);
-    }
 }
